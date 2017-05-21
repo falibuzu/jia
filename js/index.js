832f2f864d9a46fa57bd105bf1409a5175a35417
falibuzu <falibuzu@sina.com>
2017-05-22 03:38:56 +0800

    方法

---
+++ js/index.js
@@ -0,0 +1,56 @@
+
+	
+	
+$(function () {
+/*
+$(".tp_list a").mouseenter(function(){
+	$(this).addClass("on").siblings().removeClass("on");			
+})
+$(".tp_list a").hover(function(){
+	$(this).addClass("on");
+	},function(){
+	$(this).removeClass("on")	
+})
+
+*/
+
+
+$(".ico_play li").hover(function(){
+
+	$(this).find(".ic").stop().animate({width:"0"},"fast").animate({width:"26px"},"fast");
+	},function(){
+	$(this).find(".ic").stop().animate({width:"0"},"fast").animate({width:"26px"},"fast");
+});
+
+/*
+
+$(".new_nav").find( "li").each(function(index){
+       $(this).mouseenter(
+       function(){
+		 $(this).siblings().removeClass("on");
+		 $(this).addClass("on");
+		 
+		 $(this).parent(".new_nav").next(".change_s").find( ".change_box").hide();
+		 $(this).parent(".new_nav").next(".change_s").find( ".change_box:eq(" + index + ")").show();
+		alert(index)
+
+      })
+   })
+*/
+$(".new_nav").each(function(){
+       $(this).find("li").each(function(index){
+        $(this).mouseenter(
+		   function(){
+			 $(this).siblings().removeClass("on");
+			 $(this).addClass("on");
+			 
+			 $(this).parent(".new_nav").next(".change_s").find( ".change_box").slideUp();
+			 $(this).parent(".new_nav").next(".change_s").find( ".change_box:eq(" + index + ")").fadeIn();
+			
+	
+		 })
+   })
+ })
+$('.ad_jdt').hiSlider();
+
+});
